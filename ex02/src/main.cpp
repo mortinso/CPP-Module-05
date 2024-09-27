9bc03ac8df9f9a0d556b27d02bc6912d1f81a9c2
mortinso <89762267+mortinso@users.noreply.github.com>
2024-09-27 16:44:14 +0100

    ex02: Reorganized main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 15:12:40 by mortins-          #+#    #+#             */
-/*   Updated: 2024/09/27 16:29:30 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/09/27 16:43:45 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,71 +19,71 @@
 int	main(void) {
 	Bureaucrat crash("Crash Bandicoot", 1);
 	Bureaucrat fake_crash("Fake Crash", 150);
-	PresidentialPardonForm pp_form("Doctor Neo Cortex");
-	RobotomyRequestForm rr_form("Aku Aku");
 	ShrubberyCreationForm sc_form("Cortex Island");
+	RobotomyRequestForm rr_form("Aku Aku");
+	PresidentialPardonForm pp_form("Doctor Neo Cortex");
 	
 	{ // Form Info
 		std::cout << GREEN << "Form Info" << RESET << std::endl;
-		std::cout << pp_form << std::endl;
-		std::cout << rr_form << std::endl;
 		std::cout << sc_form << std::endl;
+		std::cout << rr_form << std::endl;
+		std::cout << pp_form << std::endl;
 	}
 	{ // Failed Signing Test
 		std::cout << std::endl << GREEN << "Failed Signing Test" << RESET << std::endl;
-		try { fake_crash.signForm(pp_form); }
+		try { fake_crash.signForm(sc_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
 		try { fake_crash.signForm(rr_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
-		try { fake_crash.signForm(sc_form); }
+		try { fake_crash.signForm(pp_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 	}
 	{ // Unsigned Execution Test
 		std::cout << std::endl << GREEN << "Unsigned Execution Test" << RESET << std::endl;
-		try { fake_crash.executeForm(pp_form); }
+		try { fake_crash.executeForm(sc_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
 		try { fake_crash.executeForm(rr_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
-		try { fake_crash.executeForm(sc_form); }
+		try { fake_crash.executeForm(pp_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 	}
 	{ // Signing Test
 		std::cout << std::endl << "---------------------------------------------" << std::endl;
 		std::cout << GREEN << "Form Signing" << RESET << std::endl;
-		try { crash.signForm(pp_form); }
+		try { crash.signForm(sc_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
 		try { crash.signForm(rr_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
-		try { crash.signForm(sc_form); }
+		try { crash.signForm(pp_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 		std::cout << "---------------------------------------------" << std::endl;
 	}
 	{ // Failed Execution Test
 		std::cout << std::endl << GREEN << "Failed Execution Test" << RESET << std::endl;
-		try { fake_crash.executeForm(pp_form); }
+		try { fake_crash.executeForm(sc_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
 		try { fake_crash.executeForm(rr_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 
-		try { fake_crash.executeForm(sc_form); }
+		try { fake_crash.executeForm(pp_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 	}
 	{ // Execution Test
 		std::cout << std::endl << GREEN << "Execution Test" << RESET << std::endl;
-		try { crash.executeForm(pp_form); }
+		try { crash.executeForm(sc_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 		std::cout << std::endl;
 		try { crash.executeForm(rr_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 		std::cout << std::endl;
-		try { crash.executeForm(sc_form); }
+		try { crash.executeForm(pp_form); }
 		catch (const std::exception &e) { std::cout << e.what() << std::endl; }
 	}
 /* 	{ // 50% chance Test
